bcb7ccf5df58fafd8fc67415952861e0574ec641
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-21 17:36:05 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -24,4 +24,8 @@ class PigLatinizer
   def consonant?(char)
     !char.match(/[aAeEiIoOuU]/)
   end
+  
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
 end
