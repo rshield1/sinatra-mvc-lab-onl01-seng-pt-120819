c9312fbfde56b66001be543f7c448a265292048a
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-21 17:39:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,9 @@ class App < Sinatra::Base
    end
    
    post '/piglatinize' do
-     
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
      
    end
 end
